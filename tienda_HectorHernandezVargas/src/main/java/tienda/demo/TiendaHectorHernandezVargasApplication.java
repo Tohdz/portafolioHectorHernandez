7a014f8d522fca507aeeb1b22e8d54a1d8abde05
Tohdz <hhernandez70088@ufide.ac.cr>
2024-01-24 22:56:35 -0600

    Html practice
    sem1/labels/practice

---
+++ tienda_HectorHernandezVargas/src/main/java/tienda/demo/TiendaHectorHernandezVargasApplication.java
@@ -0,0 +1,14 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaHectorHernandezVargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaHectorHernandezVargasApplication.class, args);
+                
+	}
+
+}
