d4fc28779b9309d89bafafbe693e408fd847f97d
Tohdz <hhernandez70088@ufide.ac.cr>
2024-02-05 22:55:01 -0600

    reinstall  Net SB
    spring boot

--- tienda_HectorHernandezVargas/src/main/java/tienda/demo/TiendaHectorHernandezVargasApplication.java
+++ tienda_HectorHernandezVargas/src/main/java/tienda/demo/TiendaHectorHernandezVargasApplication.java
@@ -2,10 +2,17 @@ package tienda.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class TiendaHectorHernandezVargasApplication {
-
+        @GetMapping("/hello")
+        public String hello(){
+            return "hola mundo tooo";
+        }
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaHectorHernandezVargasApplication.class, args);
                 
