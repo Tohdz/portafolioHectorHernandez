bfb8cc15a08a787011c51a8a78282f8f892178f6
Tohdz <hhernandez70088@ufide.ac.cr>
2024-02-18 19:16:56 -0600

    Segunda parte semana 5
    hhernan

--- tienda_HectorHernandezVargas/src/main/java/tienda/demo/ProjectConfig.java
+++ tienda_HectorHernandezVargas/src/main/java/tienda/demo/ProjectConfig.java
@@ -4,10 +4,45 @@
  */
 package tienda.demo;
 
-/**
- *
- * @author hhern
- */
-public class ProjectConfig {
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.il8n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.il8n.SessionLocaleResolver;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer{
+    @Bean
+    public LocaleResolver localeResolver () {
+        var slr = new SessionLocaleResolver ();
+        slr.setDefaultLocale (Locale.getDefault());
+        slr.setLocaleAttributeName ("session.current.locale");
+        slr.setTimeZoneAttributeName ( "session.current.timezone");
+        return slr;
+    }
     
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor () {
+        var lci = new LocaleChangeInterceptor ();
+        lci.setParamName ("lang");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor (localeChangeInterceptor());
+    }
+    
+    @Bean ("messageSource")
+    public MessageSource messageSource () {
+        ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource ();
+        messageSource.setBasenames ( "messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
